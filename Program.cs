9687bd896effed5faa6bfcb5efd4170bf163d234
rosine ngono <ngrosine07@gmail.com>
2024-12-19 04:26:26 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace DB_toDoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choix = 1, reponse = 1;
+            string setting = "Server=localhost;Database=todolist_db;Uid=root;Pwd=rosine28;";
+            try
+            {
+                using(MySqlConnection connection=new MySqlConnection(setting))
+                {
+                    connection.Open();
+                    Console.WriteLine("connection ouverte");
+                    do
+                    {
+                        Console.WriteLine(@"bienvenue sur db-todolist,que voulez vous faire?
+                        1-ajouter une tache
+                        2-supprimer une tache
+                        3-marquer une tache comme terminer
+                        4-afficher la liste des taches
+                        5-afficher les taches terminees");
+                        choix=int.Parse(Console.ReadLine());
+                        switch(choix)
+                        {
+                            case 1:
+                                Console.WriteLine("entrer l'intitule de la tache");
+                                string intitule=Console.ReadLine();
+                                Console.WriteLine("entrer l'heure de debut de la tache format" +
+                                    "heure:minute:seconde exemple 10:15:00");
+                                string h_debut = Console.ReadLine();
+                                Console.WriteLine("entrer l'heure de fin de la tache format " +
+                                    "heure:minute:seconde exemple 10:15:00");
+                                                                        
+                                string h_fin = Console.ReadLine();
+                                Task tache=new Task(intitule, h_debut, h_fin);
+                                Console.WriteLine("tache cree");
+                                tache.AddTask(connection);
+                                break;
+                            case 2:
+                                Console.WriteLine("entrer l'intitule de la tache a supprimer");
+                                 intitule = Console.ReadLine();
+                                Task.DeleteTask(connection, intitule);
+                                break;
+                            case 3:
+                                Console.WriteLine("entrer l'intitule de la tache terminee");
+                                intitule = Console.ReadLine();
+                                Task.MarkAsFinish(connection, intitule);
+                                break;
+                            case 4:
+                                Task.ListTask(connection);
+                                break;
+                            case 5:
+                                Task.NotFinishTask(connection);
+                                break;
+                            default: 
+                                Console.WriteLine("choisissez un chiffres correspondant aux " +
+                                    "actions ci dessous");
+                                break;
+
+
+                        }
+                        Console.WriteLine(@"avez vous besoin d'effectuer une autre action?
+                                            1-oui
+                                            2-non");
+                        reponse=int.Parse(Console.ReadLine());
+
+                    } while (reponse == 1); 
+                }
+            }
+            catch (MySqlException e) 
+            { 
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}   
---
+++ Task.cs
@@ -0,0 +1,88 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_toDoList
+{
+    internal class Task
+    {
+        public string? intitule { get;private set; }
+        public string heure_debut { get; private set; }
+        public string heure_fin { get; private set; }
+        public int finish { get; private set; }
+        public Task(string intitule,string heure_debut,string heure_fin)
+        {
+            this.intitule = intitule;
+            this.heure_debut = heure_debut;
+            this.heure_fin = heure_fin;
+            this.finish = 0;
+            
+        }
+        public static void ListTask(MySqlConnection connection)
+        {
+            string query = "select * from task";
+            using(MySqlCommand cmd = new MySqlCommand(query,connection))
+            {
+                using(MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader["intitule"]);
+                    }
+                }
+            }
+        }
+        public static void MarkAsFinish(MySqlConnection connection,string tache)
+        {
+            string query = "update task set termine=true where intitule=@tache";
+            using(MySqlCommand cmd= new MySqlCommand(query,connection))
+            {
+                cmd.Parameters.AddWithValue("@tache",tache);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public static void DeleteTask(MySqlConnection connection,string tache)
+        {
+            string query = "delete from task where intitule=@tache";
+            using (MySqlCommand cmd= new MySqlCommand(query,connection))
+            {
+                cmd.Parameters.AddWithValue("@tache", tache);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public static void NotFinishTask(MySqlConnection connection)
+        {
+            string query = "select * from task where termine=1";
+            using (MySqlCommand cmd= new MySqlCommand(query,connection))
+            {
+                using(MySqlDataReader rd = cmd.ExecuteReader())
+                { while (rd.Read())
+                    {
+                        Console.WriteLine(rd["intitule"]);
+                    }
+                        
+                            
+                }
+            }
+        }
+        public void AddTask(MySqlConnection connection)
+        {
+            string query = "insert into task(intitule,termine,heure_debut,heure_final) values(@intitule,@termine,@heure_debut,@heure_final)";
+            using(MySqlCommand cmd=new MySqlCommand(query,connection))
+            {
+                cmd.Parameters.AddWithValue("@intitule", this.intitule);
+                cmd.Parameters.AddWithValue("@termine", this.finish);
+                cmd.Parameters.AddWithValue("@heure_debut", this.heure_debut);
+                cmd.Parameters.AddWithValue("@heure_final", this.heure_fin);
+                
+                cmd.ExecuteNonQuery ();
+
+            }
+        }
+    }
+}
